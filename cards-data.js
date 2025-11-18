cae6c12ada92e3bc17aa41232f4a26fd3b73d63d
jman1593 <95952762+jman1593@users.noreply.github.com>
2025-11-18 12:51:24 -0700

    Approve game: Update cards-data.js

--- cards-data.js
+++ cards-data.js
@@ -1,4 +1,5 @@
 export default [
+  { href: './html/Jelly Bots/index.html', imgSrc: 'jelly bots.jpeg', name: 'Jelly Bots' , 'page': 5, 'id': 891 },
   { href: './html/Jolly world/index.html', imgSrc: 'jolly world.jpeg', name: 'Jolly world' , 'page': 5, 'id': 893 },
   { href: './html/Like a king/index.html', imgSrc: 'like a king.jpeg', name: 'Like a king' , 'page': 5, 'id': 910 },
   { href: './html/One trick mage/index.html', imgSrc: 'one trick mage.jpeg', name: 'One trick mage' , 'page': 5, 'id': 912 },
