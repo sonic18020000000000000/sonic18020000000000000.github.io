db3ca6146e51acc6d7e7d4f7a4fbd44d5095c4e4
jman1593 <josh@pettits.net>
2025-12-01 21:22:49 -0700

    add panic button

---
+++ js/panicButton.js
@@ -0,0 +1,33 @@
+let panicRedirectFromStorage = localStorage.getItem('panicRedirect')
+let panicUrlEdited = "canvas.com"
+const panicKeysArray = JSON.parse(localStorage.getItem('panicKeys'))
+const keyDownSet = new Set();
+
+if(panicRedirectFromStorage) {
+    panicUrlEdited = panicRedirectFromStorage.replace(/^https?:\/\//, '');
+}
+
+document.addEventListener('keydown', function(e) {
+    if (e.ctrlKey || e.altKey || e.metaKey) {
+        e.preventDefault();
+        e.stopPropagation();
+    }
+})
+
+document.addEventListener('keydown', function(e){
+    keyDownSet.add(e.key)
+    checkPanicKeys();
+})
+
+document.addEventListener('keyup', function(e){
+    keyDownSet.delete(e.key)
+})
+
+function checkPanicKeys() {
+    if(!panicKeysArray) return
+    const allDown = panicKeysArray.every(key => keyDownSet.has(key));
+    if (allDown) {
+        window.location.href = `https://${panicUrlEdited}`
+    }
+}
+console.log("injected")
